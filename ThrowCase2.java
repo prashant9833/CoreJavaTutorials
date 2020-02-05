a68bb56e63004d107270f6ac0ca046c667b8e98f
Prashant <prashanttiw983@gmail.com>
2020-02-06 00:32:29 +0530

    Created a Case-2 code for Throw Keyword------(2)

---
+++ ThrowCase2.java
@@ -0,0 +1,10 @@
+public class ThrowCase2
+{
+  public static void main(String args[])
+{
+   throw new ArithmeticException("Message***");  
+
+ System.out.println("Hello Prashant!!!!");  // Here we got C.E. Unreachable Statement.
+
+}
+}
