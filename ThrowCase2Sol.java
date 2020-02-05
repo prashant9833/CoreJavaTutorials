cde173d51656cb1ac6af26bc3a938cb32666242d
Prashant <prashanttiw983@gmail.com>
2020-02-06 01:08:48 +0530

    Changes has been done for Throw Case-2 solutions------

---
+++ ThrowCase2Sol.java
@@ -0,0 +1,19 @@
+public class ThrowCase2Sol
+{
+public static void main(String args[])
+{
+String s=args[0];
+
+throw new ArithmeticException("Message///");
+
+int x=10;
+
+int y=20;
+
+int z=x+y;
+
+System.out.println(z);
+
+}
+
+}
